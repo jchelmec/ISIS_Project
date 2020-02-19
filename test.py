1620c503ff683f52859c30bfb5f9aadf6e283428
Jarek <jchelmec@gmail.com>
2020-02-19 15:11:26 +0100

    Działające wypadkowa z kanalow

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import openData
+import fileDirSearch
 from pathlib import Path
 
+file = fileDirSearch.fileDir('*.txt')
 
-
-filename = '001-B1.txt'
-data_obj = openData.LoadData()
-data_obj.load_from_file(filename)
-data_obj.detect_hummer_data(channel=2, sens=0.3 , len_imp=2, pre=0.2, post=3.8, plot = False)
-data_obj.save_to_file()
+for x in file:
+    # filename = '001-B1.txt'
+    data_obj = openData.LoadData()
+    data_obj.load_from_file(str(x))
+    data_obj.detect_hummer_data(channel=4, sens=0.3 , len_imp=2, pre=0.2, post=3.8, plot = True)
+    data_obj.save_to_file()
 
 
 
--- wypadkowaKanalow.py
+++ wypadkowaKanalow.py
@@ -4,36 +4,87 @@ import pandas as pd
 class Wypadkowa:
     def __init__(self, filename):
         self.filename = filename
+        self.dane = pd.read_csv(self.filename, sep='\t')
 
-    def wyborKanalow(self, kanaly):
+
+    def wyborKanalow(self, kanaly:int):
         """Odczyt wybranych kanałów"""
-        self.dane = pd.read_csv(self.filename, sep='\t', usecols=kanaly)
-        print(self.dane.iloc[0:10,:])
+        self.dane =self.dane.filter(self.dane.iloc[:,kanaly])
         return self.dane
 
-    def wypadkowaKanalow(self,kanaly):
+    def wypadkowaKanalow(self,kanaly:int):
         """Obliczanie wypadkowej wybranych kanałów kanalów"""
 
-        print(self.dane.pop(self.dane.columns[kanaly]))
-        # dane_wyp = self.dane.pop(self.dane.columns[kanaly])
-        # print(dane_wyp.iloc[0:10])
-        # print(self.dane.iloc[0:10,:])
+        dane_wyp = self.dane.iloc[:,kanaly[0]]
+        for i in range(1,len(kanaly)):
+            dane_wyp += self.dane.iloc[:,kanaly[i]]
+        return dane_wyp / len(kanaly)
 
-    def skalowanieKanalow(self, kanal):
+    def skalowanieKanalow(self, kanal:int, mnoznik:int):
         """Skalowanie kanałów"""
+        self.dane.iloc[:,kanal] *= mnoznik
+        return self.dane
 
+    def getData(self):
+        return self.dane
+
+class PrepData:
+    def __init__(self,filename):
+        self.filename = filename
+
+
+        f = Wypadkowa(filename)
+
+        # dane_wejsciowe = f.getData()
+        # print('-----------------------------')
+        # print ('dane wejsciowe')
+        # print('-----------------------------')
+        # print(dane_wejsciowe)
+
+        # kan = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,31]
+        # kan =[]
+        # for x in range(1,17): kan.append(x)
+        # kan.append(31)
+
+        kan = [0,1,2,3,4,5,6]
+
+        dane_wybrane = f.wyborKanalow(kan)
+
+        # print('------------------------------')
+        # print('Dane wybrane')
+        # print('------------------------------')
+        # print(dane_wybrane)
+
+
+        # f.wypadkowaKanalow(kan_wypad)
+        for i in range (1,16):
+            f.skalowanieKanalow(i,2)        # mnożnik do skalowania plików
+
+        dane_wyskalowane = f.getData()
+
+        # print('------------------------------')
+        # print('Dane wyskalowane')
+        # print('------------------------------')
+        # print(dane_wyskalowane)
+
+        t= f.getData().iloc[:,0]
+        kan_wypad = [4,5,6]
+        s1 = f.wypadkowaKanalow(kan_wypad)
+        kan_wypad = [5,6]
+        s2= f.wypadkowaKanalow(kan_wypad)
+        kan_wypad = [6,10]
+        s3 = f.wypadkowaKanalow(kan_wypad)
+
+        self.dane_wypadkowe = pd.concat([t, s1, s2, s3], axis= 1 )
+
+        print('------------------------------')
+        print('Dane wypadkowe')
+        print('------------------------------')
+        print(self.dane_wypadkowe)
+
+    def saveData(self):
+        pd.
 
 filename='dane_testowe.txt'
-f = Wypadkowa(filename)
 
-# kan = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,31]
-# kan =[]
-# for x in range(1,17): kan.append(x)
-# kan.append(31)
-
-kan = [0,1,2,3,4,5,6]
-print(kan)
-f.wyborKanalow(kan)
-kan_wypad = [1,2,3,4]
-f.wypadkowaKanalow(kan_wypad)
-
+PrepData(filename)
